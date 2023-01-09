341510b1a34ee5b3b692f9c9aab22627371efde3
doris hermawan <dorizo89@gmail.com>
2023-01-09 17:59:01 +0700

    sss

--- __service-worker.js
+++ __service-worker.js
@@ -4,7 +4,7 @@
 //--------------------------------------------------------------------------
 
 // Version
-var VERSION = 2.1
+var VERSION = 2.2
 
 // Cache name
 var CACHE_NAME = 'cache-version-' + VERSION;
